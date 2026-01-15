51d47ae9b26d0b0a41613298491dcc49f187178e
p20316 <p203166@sw.hs.kr>
2026-01-15 13:03:46 +0900

    Remove duplicate font settings in gamjung.py
    Removed duplicate font settings for Korean characters and unicode minus.

--- gamjung.py
+++ gamjung.py
@@ -4,23 +4,6 @@ import matplotlib.pyplot as plt
 
 
 
-# 한글 폰트 설정 (윈도우 기준)
-font_path = "C:/Windows/Fonts/malgun.ttf"
-font_name = fm.FontProperties(fname=font_path).get_name()
-mpl.rc('font', family=font_name)
-
-# 마이너스 기호 깨짐 방지
-mpl.rcParams['axes.unicode_minus'] = False
-
-
-# 한글 폰트 설정 (윈도우 기준)
-font_path = "C:/Windows/Fonts/malgun.ttf"
-font_name = fm.FontProperties(fname=font_path).get_name()
-mpl.rc('font', family=font_name)
-
-# 마이너스 기호 깨짐 방지
-mpl.rcParams['axes.unicode_minus'] = False
-
 
 # =====================
 # 감정별 키워드와 공감 답변
